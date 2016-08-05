14d1923000b16dd66fe54761dff7c96a56e98658
Cam Gibson <gibson.compsci@gmail.com>
2016-08-05 11:52:08 -0700

    Move IV byte skipping out of AEAD GCM crypto functions

--- crypto/s2n_aead_cipher_aes_gcm.c
+++ crypto/s2n_aead_cipher_aes_gcm.c
@@ -25,7 +25,7 @@
 
 static int s2n_aead_cipher_aes_gcm_encrypt(struct s2n_session_key *key, struct s2n_blob *iv, struct s2n_blob *aad, struct s2n_blob *in, struct s2n_blob *out)
 {
-    gte_check(in->size, S2N_TLS_GCM_TAG_LEN + S2N_TLS_GCM_EXPLICIT_IV_LEN);
+    gte_check(in->size, S2N_TLS_GCM_TAG_LEN);
     gte_check(out->size, in->size);
     eq_check(iv->size, S2N_TLS_GCM_IV_LEN);
     eq_check(aad->size, S2N_TLS_GCM_AAD_LEN);
@@ -36,9 +36,7 @@ static int s2n_aead_cipher_aes_gcm_encrypt(struct s2n_session_key *key, struct s
     }
 
     /* Adjust our buffer pointers to account for the explicit IV and TAG lengths */
-    int in_len = in->size - (S2N_TLS_GCM_EXPLICIT_IV_LEN + S2N_TLS_GCM_TAG_LEN);
-    uint8_t *in_data = in->data + S2N_TLS_GCM_EXPLICIT_IV_LEN;
-    uint8_t *out_data = out->data + S2N_TLS_GCM_EXPLICIT_IV_LEN;
+    int in_len = in->size - S2N_TLS_GCM_TAG_LEN;
     uint8_t *tag_data = out->data + out->size - S2N_TLS_GCM_TAG_LEN;
 
     int out_len;
@@ -48,12 +46,12 @@ static int s2n_aead_cipher_aes_gcm_encrypt(struct s2n_session_key *key, struct s
     }
 
     /* Encrypt the data */
-    if (EVP_EncryptUpdate(&key->native_format.evp_cipher_ctx, out_data, &out_len, in_data, in_len) != 1) {
+    if (EVP_EncryptUpdate(&key->native_format.evp_cipher_ctx, out->data, &out_len, in->data, in_len) != 1) {
         S2N_ERROR(S2N_ERR_ENCRYPT);
     }
 
     /* Finalize */
-    if (EVP_EncryptFinal_ex(&key->native_format.evp_cipher_ctx, out_data, &out_len) != 1) {
+    if (EVP_EncryptFinal_ex(&key->native_format.evp_cipher_ctx, out->data, &out_len) != 1) {
         S2N_ERROR(S2N_ERR_ENCRYPT);
     }
 
@@ -67,7 +65,7 @@ static int s2n_aead_cipher_aes_gcm_encrypt(struct s2n_session_key *key, struct s
 
 static int s2n_aead_cipher_aes_gcm_decrypt(struct s2n_session_key *key, struct s2n_blob *iv, struct s2n_blob *aad, struct s2n_blob *in, struct s2n_blob *out)
 {
-    gte_check(in->size, S2N_TLS_GCM_TAG_LEN + S2N_TLS_GCM_EXPLICIT_IV_LEN);
+    gte_check(in->size, S2N_TLS_GCM_TAG_LEN);
     gte_check(out->size, in->size);
     eq_check(iv->size, S2N_TLS_GCM_IV_LEN);
     eq_check(aad->size, S2N_TLS_GCM_AAD_LEN);
@@ -78,9 +76,7 @@ static int s2n_aead_cipher_aes_gcm_decrypt(struct s2n_session_key *key, struct s
     }
 
     /* Adjust our buffer pointers to account for the explicit IV and TAG lengths */
-    int in_len = in->size - (S2N_TLS_GCM_EXPLICIT_IV_LEN + S2N_TLS_GCM_TAG_LEN);
-    uint8_t *in_data = in->data + S2N_TLS_GCM_EXPLICIT_IV_LEN;
-    uint8_t *out_data = out->data + S2N_TLS_GCM_EXPLICIT_IV_LEN;
+    int in_len = in->size - S2N_TLS_GCM_TAG_LEN;
     uint8_t *tag_data = in->data + in->size - S2N_TLS_GCM_TAG_LEN;
 
     /* Set the TAG */
@@ -95,12 +91,12 @@ static int s2n_aead_cipher_aes_gcm_decrypt(struct s2n_session_key *key, struct s
     }
 
     /* Decrypt the data */
-    if (EVP_DecryptUpdate(&key->native_format.evp_cipher_ctx, out_data, &out_len, in_data, in_len) != 1) {
+    if (EVP_DecryptUpdate(&key->native_format.evp_cipher_ctx, out->data, &out_len, in->data, in_len) != 1) {
         S2N_ERROR(S2N_ERR_DECRYPT);
     }
 
     /* Verify the tag */
-    if (EVP_DecryptFinal_ex(&key->native_format.evp_cipher_ctx, out_data, &out_len) != 1) {
+    if (EVP_DecryptFinal_ex(&key->native_format.evp_cipher_ctx, out->data, &out_len) != 1) {
         S2N_ERROR(S2N_ERR_DECRYPT);
     }
 
